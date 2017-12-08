7506a13aff5004850868c1e82c5f1060b8d91d3a
Lucas Tolchinsky <tonchis@protonmail.com>
2017-12-08 12:50:02 -0300

    Add tests.

---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFilenameOnly(t *testing.T) {
+	link := resolveLink("", "foo.go", "", "")
+	commit := bashExec("git rev-parse HEAD")
+	expectedLink := fmt.Sprintf("https://gitlab.com/tonchis/dvcs-link/blob/%v/foo.go", commit)
+
+	if link != expectedLink {
+		t.Errorf("Expected %v to equal %v", link, expectedLink)
+	}
+}
+
+func TestWithLine(t *testing.T) {
+	link := resolveLink("", "foo.go", "5", "")
+	commit := bashExec("git rev-parse HEAD")
+	expectedLink := fmt.Sprintf("https://gitlab.com/tonchis/dvcs-link/blob/%v/foo.go#L5", commit)
+
+	if link != expectedLink {
+		t.Errorf("Expected %v to equal %v", link, expectedLink)
+	}
+}
+
+func TestWithRange(t *testing.T) {
+	link := resolveLink("", "foo.go", "5", "10")
+	commit := bashExec("git rev-parse HEAD")
+	expectedLink := fmt.Sprintf("https://gitlab.com/tonchis/dvcs-link/blob/%v/foo.go#L5-L10", commit)
+
+	if link != expectedLink {
+		t.Errorf("Expected %v to equal %v", link, expectedLink)
+	}
+}
+
+func TestWithHost(t *testing.T) {
+	link := resolveLink("github", "foo.go", "5", "10")
+	commit := bashExec("git rev-parse HEAD")
+	expectedLink := fmt.Sprintf("https://github.com/tonchis/dvcs-link/blob/%v/foo.go#L5-L10", commit)
+
+	if link != expectedLink {
+		t.Errorf("Expected %v to equal %v", link, expectedLink)
+	}
+}
